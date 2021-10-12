91f79914563c8bc8cc2c7c5a10569551615a422e
Juliana Hildebrandt <juliana.hildebrandt@tu-dresden.de>
2021-10-12 09:43:53 +0200

    documentation

--- language/collate/Concepts.h
+++ language/collate/Concepts.h
@@ -8,7 +8,7 @@ namespace LCTL {
     /**
    * @brief Static Tokenizer is a container for a constant number (tokensize)
    * 
-   * @param <I> Tokensize
+   * @tparam I Tokensize (number of values)
    * 
    * @date: 25.05.2021 12:00
    * @author: Juliana Hildebrandt
@@ -20,10 +20,10 @@ namespace LCTL {
    * @brief Recursion concept in Collate
    * 
    * Each recursion concept contains
-   * @param <tokenizer_t> a tokenizer,
-   * @param <parameterCalculator_t> a parameter calculator,
-   * @param <recursion_t> a further recursion or encoder and
-   * @param <combiner_t> a combiner. 
+   * @tparam tokenizer_t a tokenizer,
+   * @tparam parameterCalculator_t a parameter calculator,
+   * @tparam recursion_t a further recursion or encoder and
+   * @tparam combiner_t a combiner. 
    * No functionality included.
    * 
    * @date: 25.05.2021 12:00
@@ -72,9 +72,9 @@ namespace LCTL {
    * 
    * @see formats/delta/delta.h
    * 
-   * @param <prameterDefinition_t>  a parameterdefinition with name, logical, and physical value
-   * @param <start_value_t>         value that holds from the beginning (initialization before the loop)
-   * @param <recursionLevel>        each parameter belongs to a recursion level. 
+   * @tparam prameterDefinition_t  a parameterdefinition with name, logical, and physical value
+   * @tparam start_value_t         value that holds from the beginning (initialization before the loop)
+   * @tparam recursionLevel        each parameter belongs to a recursion level. 
    * 
    * @todo -1 means that the recusion level is not specified explicitely. But this should not make a difference.
    * 0 means, that the parameter belongs to the outer recursion. Maybe we never need the recursionlevel here.
@@ -88,7 +88,7 @@ namespace LCTL {
   /**
    * @brief the parameter calculator contains zero, one or several parameter definitions
    * 
-   * @param <pads>  parameterdefinitions
+   * @tparam pads  parameterdefinitions
    * 
    * @date: 26.05.2021 12:00
    * @author: Juliana Hildebrandt
@@ -108,8 +108,8 @@ namespace LCTL {
   /**
    * @brief each encoder transforms finite tokens, which shall not be splittet anymore.
    * 
-   * @param <logicalPreprocessing_t>  logical calculation rule to process the input data at a mathematical level
-   * @param <numberOfBits_t>          physical calculation rule to calculate the a bitstring to store the data
+   * @tparam logicalPreprocessing_t  logical calculation rule to process the input data at a mathematical level
+   * @tparam numberOfBits_t          physical calculation rule to calculate the a bitstring to store the data
    * 
    * @todo at the moment it is only possible to have a bitwidth as physical calculation rule. 
    * But it might be useful to process the logical values somehow before the physical encoding,
@@ -125,8 +125,8 @@ namespace LCTL {
   /**
    * @brief defines the storage layout
    * 
-   * @param <func_combine>  contains a function to combine data and parameters
-   * @param <aligned>       defines, if each block starts at a word (resp. register) border or not
+   * @tparam func_combine  contains a function to combine data and parameters
+   * @tparam aligned       defines, if each block starts at a word (resp. register) border or not
    * 
    * @todo we need a combiner with several output tracks. Each output track has to have its own processing style,
    * i.e. SIMD for data, scalar for descriptors
